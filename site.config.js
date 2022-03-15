d40fc937b534d81919cd996fa327f69e2397cb09
wuxiaofei98 <85546874+wuxiaofei98@users.noreply.github.com>
2022-03-15 14:31:18 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'abb559591c1744819d0f6a13d8233a67',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '吴小飞的 个人博客',
+  domain: 'https://foil-comfort-d24.notion.site/abb559591c1744819d0f6a13d8233a67',
+  author: '吴 小飞',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,10 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+//   twitter: 'transitive_bs',
+  E-mail: 'wuchen98@foxmail.com',
+  Github: 'wuxiaofei98',
+//   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
